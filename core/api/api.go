ce69d56dae1ffc204457fed420f827d21bb69eab
D <r@example.com>
2021-06-12 23:02:56 +0300

    add core api

---
+++ core/api/api.go
@@ -0,0 +1,90 @@
+package api
+
+import (
+	"context"
+
+	"github.com/rs/zerolog/log"
+
+	desc "github.com/ozoncp/ocp-progress-api/pkg/ocp-progress-api"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+const (
+	errProjectListEmpty = "not found any projects"
+	errProjectNotFound  = "project not found"
+	errProjectCreate    = "creating project fails"
+	errProjectRemove    = "removing project fails"
+)
+
+type api struct {
+	desc.UnimplementedOcpProgressApiServer
+}
+
+func NewOcpProgressApi() desc.OcpProgressApiServer {
+	return &api{}
+}
+
+func (a *api) CreateProgressV1(
+	ctx context.Context,
+	req *desc.CreateProgressV1Request) (
+	*desc.CreateProgressV1Response,
+	error) {
+
+	if err := req.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	log.Info().Msgf("Got CreateProgressV1: {ClassroomId: %d, UserId: %d}", req.ClassroomId, req.UserId)
+
+	err := status.Error(codes.NotFound, errProjectCreate)
+	return nil, err
+}
+
+func (a *api) DescribeProgressV1(
+	ctx context.Context,
+	req *desc.DescribeProgressV1Request) (
+	*desc.DescribeProgressV1Response,
+	error) {
+
+	if err := req.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	log.Info().Msgf("Got DescribeProgressV1: {ProgressId: %d}", req.ProgressId)
+	err := status.Error(codes.NotFound, errProjectCreate)
+	return nil, err
+}
+
+func (a *api) ListProgresssV1(
+	ctx context.Context,
+	req *desc.ListProgresssV1Request) (
+	*desc.ListProgresssV1Response,
+	error) {
+
+	if err := req.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	log.Info().Msgf("Got ListProgresssV1: {limit: %d, offset: %d}", req.Limit, req.Offset)
+
+	err := status.Error(codes.NotFound, errProjectListEmpty)
+	return nil, err
+}
+
+func (a *api) RemoveProgressV1(
+	ctx context.Context,
+	req *desc.RemoveProgressV1Request) (
+	*desc.RemoveProgressV1Response,
+	error) {
+
+	if err := req.Validate(); err != nil {
+		return nil, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	log.Info().Msgf("Got RemoveProgressV1: {ProgressId: %d}", req.ProgressId)
+
+	err := status.Error(codes.NotFound, errProjectRemove)
+	return nil, err
+
+}
